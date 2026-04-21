2686708afb23c054d129b25ff58b571d4ced57a9
anjanisolanki <anjanisolanki1220@gmail.com>
2026-04-21 17:53:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,4 +35,4 @@ class Teacher extends Person {
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
-window.Teacher = Teacher;cher;
+window.Teacher = Teacher;
